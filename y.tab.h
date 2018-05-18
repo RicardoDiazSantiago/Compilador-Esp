1fc2c9d2fbbc555b15f007f78c5adf47cb28c44f
kareemchan <kareem.chanfreau@gmail.com>
2018-05-18 11:42:21 -0500

    Update y.tab.h

--- y.tab.h
+++ y.tab.h
@@ -3,11 +3,15 @@ typedef union {
    int ival;
    string sval;
 } YYSTYPE;
-#define	ID	258
-#define	STRING	259
-#define	INT	260
-#define	DESPLIEGA 261
+#define ID	258
+#define STRING	259
+#define INT	260
+#define DESPLIEGA 261
 #define SI        262
-#define OTRO      263
+#define SINO      263
 #define ENTERO    269
+#define PRINCIPAL 270
+#define PARA      271
+#define RETORNA   272
+#define CONSTANTE 273
 extern YYSTYPE yylval;
