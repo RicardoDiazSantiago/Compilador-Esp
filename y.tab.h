180918cfa15447719036b5af0101c3189524eec0
kareemchan <kareem.chanfreau@gmail.com>
2018-05-18 11:45:18 -0500

    Update y.tab.h

--- y.tab.h
+++ y.tab.h
@@ -8,6 +8,10 @@ typedef union {
 #define	INT	260
 #define	DESPLIEGA 261
 #define SI        262
-#define OTRO      263
+#define SINO      263
 #define ENTERO    269
+#define PRINCIPAL 270
+#define PARA      271
+#define RETORNA   272
+#define CONSTANTE 273
 extern YYSTYPE yylval;
