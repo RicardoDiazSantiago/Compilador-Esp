040ce597e9a315709cde04134beb6354d43be756
AlinaMPerezSoberanes <psoberanes_mariana@hotmail.com>
2018-05-28 20:11:05 -0500

    Agregando identificador main

--- y.tab.h
+++ y.tab.h
@@ -3,11 +3,12 @@ typedef union {
    int ival;
    string sval;
 } YYSTYPE;
-#define ID	258
-#define STRING	259
-#define INT	260
-#define DESPLIEGA 261
+#define	ID	258
+#define	STRING	259
+#define	INT	260
+#define	DESPLIEGA 261
 #define SI        262
 #define OTRO      263
 #define ENTERO    269
+#define MAIN      264
 extern YYSTYPE yylval;
